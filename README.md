cd267e7df4c1b4b9d2122ac4937b969867088005
jack.huangly@gmail.com <jack.huangly@gmail.com>
2022-02-04 12:27:46 -0500

    added roc plot

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 - Project **Predict Customer Churn** of ML DevOps Engineer Nanodegree Udacity
 
 ## Project Description
-Your project description here.
-
+Predicting customer churn. Data is stored in the data folder, EDA and training results in the Images
+Folder, and model is saved in the model folder.
+`constants.py` file defines the constants used for the model
 
 ## Running Files
-How do you run your files? What should happen when you run your files?
-
+`python churn_library.py` to run the main program
+`pytest` to run the tests in test_churn_script_logging_and tests.py
 
--- test_churn_script_logging_and_tests.py
+++ test_churn_script_logging_and_tests.py
@@ -1,4 +1,8 @@
-import os
+"""
+Runs tests for the Model class defined under churn_library.pt
+Author: Jack Huang
+Date: 3-Feb-22
+"""
 import logging
 import churn_library
 import constants
@@ -6,98 +10,102 @@ import constants
 
 logging.basicConfig(
     filename='./logs/churn_library.log',
-    level = logging.INFO,
+    level=logging.ERROR,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
 
-def test_import():
-	'''
-	test data import - this example is completed for you to assist with the other test functions
-	'''
-	try:
-		model = churn_library.Model("unittest")
-		model.import_data(constants.path_rawdata)
-		logging.info("Testing import_data: SUCCESS")
-	except FileNotFoundError as err:
-		logging.error("Testing import_eda: The file wasn't found")
-		raise err
 
-	try:
-		df = model.get_df_raw()
-		assert df.shape[0] > 0
-		assert df.shape[1] > 0
-	except AssertionError as err:
-		logging.error("Testing import_data: The file doesn't appear to have rows and columns")
-		raise err
+def test_import():
+    '''
+    test data import - this example is completed for you to assist with the other test functions
+    '''
+    try:
+        model = churn_library.Model("unittest")
+        model.import_data(constants.path_rawdata)
+        logging.info("Testing import_data: SUCCESS")
+    except FileNotFoundError as err:
+        logging.error("Testing import_eda: The file wasn't found")
+        raise err
+    try:
+        df = model.get_df_raw()
+        assert df.shape[0] > 0
+        assert df.shape[1] > 0
+    except AssertionError as err:
+        logging.error(
+            "Testing import_data: The file doesn't appear to have rows and columns")
+        raise err
 
 
 def test_encoder_helper():
-	'''
-	test encoder helper
-	model
-	'''
-	try:
-		logging.info("Performing ecoder test...")
-		model = churn_library.Model("unittest")
-		model.import_data(constants.path_rawdata)
-		df = model.get_df_raw()
-		# check that df has all the columns required for econding
-		assert set(constants.cat_columns).issubset(df.columns), "Some categorical columns are missing!"
-		logging.info("    All categtorical columns are in!")
-		model.process_df()
-		logging.info("Encode success")
-	except Exception as e:
-		logging.error(e)
-		raise e
+    '''
+    test encoder helper
+    model
+    '''
+    try:
+        logging.info("Performing ecoder test...")
+        model = churn_library.Model("unittest")
+        model.import_data(constants.path_rawdata)
+        df = model.get_df_raw()
+        # check that df has all the columns required for econding
+        assert set(constants.cat_columns).issubset(
+            df.columns), "Some categorical columns are missing!"
+        logging.info("    All categtorical columns are in!")
+        model.process_df()
+        logging.info("Encode success")
+    except Exception as err:
+        logging.error(err)
+        raise err
 
 
 def test_eda():
-	'''
-	test perform eda function
-	model object must have been loaded with data and cleaned
-	'''
-	try:
-		model = churn_library.Model("unittest")
-		model.import_data(constants.path_rawdata)
-		model.process_df()
-		logging.info("Eda success!")
-	except Exception as e:
-		logging.error(e)
-		raise e
+    '''
+    test perform eda function
+    model object must have been loaded with data and cleaned
+    '''
+    try:
+        model = churn_library.Model("unittest")
+        model.import_data(constants.path_rawdata)
+        model.process_df()
+        logging.info("Eda success!")
+    except Exception as err:
+        logging.error(err)
+        raise err
 
 
 def test_perform_feature_engineering():
-	'''
-	test perform_feature_engineering
-	'''
-	try:
-		model = churn_library.Model("unittest")
-		model.import_data(constants.path_rawdata)
-		model.process_df()
-		model.perform_feature_engineering()
-		logging.info("Feature engineering success!")
-	except Exception as e:
-		logging.error(e)
-		raise e
+    '''
+    test perform_feature_engineering
+    '''
+    try:
+        model = churn_library.Model("unittest")
+        model.import_data(constants.path_rawdata)
+        model.process_df()
+        model.perform_feature_engineering()
+        logging.info("Feature engineering success!")
+    except Exception as err:
+        logging.error(err)
+        raise err
+
 
 def test_train_models():
-	'''
-	test train_models
-	'''
-	try:
-		model = churn_library.Model("unittest")
-		model.import_data(constants.path_rawdata)
-		model.process_df()
-		model.perform_feature_engineering()
-		model.train_models()
-		logging.info("Model trained!")
-	except Exception as e:
-		logging.error(e)
-		raise e
+    '''
+    test train_models
+    '''
+    try:
+        model = churn_library.Model("unittest")
+        model.import_data(constants.path_rawdata)
+        model.process_df()
+        model.perform_feature_engineering()
+        model.train_models()
+        logging.info("Model trained!")
+    except Exception as err:
+        logging.error(err)
+        raise err
 
 
-
-
-
-
-
+if __name__ == '__main__':
+    test_import()
+    test_encoder_helper()
+    test_eda()
+    test_perform_feature_engineering()
+    test_train_models()
